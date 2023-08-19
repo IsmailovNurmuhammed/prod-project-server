b590cf2f1ada5016f709a8625de49699a70b6923
Nurmuhammed <ismailovnurmuhammed@gmail.com>
2023-08-19 18:35:54 +0600

    add server

--- api/server.js
+++ api/server.js
@@ -1,19 +1,59 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const fs = require("fs");
+const jsonServer = require("json-server");
+const path = require("path");
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+const server = jsonServer.create();
 
-// Export the Server API
-module.exports = server
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+server.use(async (req, res, next) => {
+    await new Promise((res) => {
+        setTimeout(res, 800);
+    });
+    next();
+});
+
+// Эндпоинт для логина
+server.post("/login", (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
+        );
+        const { users = [] } = db;
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password
+        );
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: "User not found" });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: "AUTH ERROR" });
+    }
+
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log("server is running on 8000 port");
+});
