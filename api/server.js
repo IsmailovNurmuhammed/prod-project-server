4224ae01af39e8dddca608695f923c89ff113111
Nurmuhammed <ismailovnurmuhammed@gmail.com>
2023-08-19 19:00:52 +0600

    fix server fake delay

--- api/server.js
+++ api/server.js
@@ -1,29 +1,18 @@
 const fs = require("fs");
 const jsonServer = require("json-server");
-const path = require("path");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const router = jsonServer.router("db.json");
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
-
-// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
-server.use(async (req, res, next) => {
-    await new Promise((res) => {
-        setTimeout(res, 800);
-    });
-    next();
-});
 
 // Эндпоинт для логина
 server.post("/login", (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(
-            fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
-        );
+        const db = JSON.parse(fs.readFileSync("db.json", "UTF-8"));
         const { users = [] } = db;
 
         const userFromBd = users.find(
